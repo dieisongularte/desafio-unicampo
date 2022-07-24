84b00c8ec3c6eaa6664d4d68b31064e9f0202109
dieisongularte <dieissongularte@gmail.com>
2022-07-24 08:48:48 -0300

    add validation_attributes

---
+++ resources/lang/pt_BR/validation_attributes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'nome_completo'   => 'Nome Completo',
+    'data_nascimento' => 'Data Nascimento',
+    'tipo'            => 'Tipo',
+    'cpf_cnpj'        => 'CPF/CNPJ',
+    'email'           => 'E-mail',
+    'endereco'        => 'Endereço',
+    'latitude'        => 'Latitude',
+    'longitude'       => 'Longitude',
+];
