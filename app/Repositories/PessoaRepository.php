16e7208b6f8292c61ef0c330e7d353f2d4380306
dieisongularte <dieissongularte@gmail.com>
2022-08-08 20:39:12 -0300

    implementa transaction para salvar registro

--- app/Repositories/PessoaRepository.php
+++ app/Repositories/PessoaRepository.php
@@ -2,16 +2,24 @@
 
 namespace App\Repositories;
 
+use App\Exceptions\CustomInternalServerErrorException;
 use App\Models\Pessoa;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PessoaRepository
 {
     public function save(Request $request)
     {
-        $requestData = $request->all();
-        $pessoa = new Pessoa;
-        $pessoa->fill($requestData)->save();
-        return $pessoa;
+        try{
+            DB::beginTransaction();
+            $pessoa = new Pessoa;
+            $pessoa->fill($request->only($pessoa->getFillable()))->save();
+            DB::commit();
+            return $pessoa;
+        }catch(\Throwable $throwable){
+            DB::rollBack();
+            throw new CustomInternalServerErrorException($throwable);
+        }
     }
 }
